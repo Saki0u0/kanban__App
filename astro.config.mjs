1da22e883de57510bec92e6ba47ee16488a4ebab
Saki Ebina <ebiebi0u0@gmail.com>
2024-11-05 23:43:39 -0800

    finished

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: 'dist',
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["*"],
+      },
+    }),
+  ],
+});
